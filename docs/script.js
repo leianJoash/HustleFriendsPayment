f578e61c79f6f2728e9162336285d35c52bb0205
leianJoash <leianjoashbernales@gmail.com>
2024-07-19 00:12:04 +0800

    beta

--- docs/script.js
+++ docs/script.js
@@ -3,17 +3,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cashOnDelivery = document.getElementById('cash-on-delivery');
     const gcash = document.getElementById('gcash');
-  
+
     if (cashOnDelivery) {
         cashOnDelivery.addEventListener('click', () => {
-            window.location.href = 'form.html';  
+            window.location.href = 'form.html';
         });
     }
-  
+
     if (gcash) {
         gcash.addEventListener('click', () => {
-            window.location.href = 'gcash.html';  
+            window.location.href = 'gcash.html';
         });
     }
-  });
-  
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('body').classList.add('transition-opacity', 'duration-1000', 'opacity-0');
+    setTimeout(() => {
+        document.querySelector('body').classList.remove('opacity-0');
+    }, 100);
+});
